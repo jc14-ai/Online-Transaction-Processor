7decb5015552847d9acc50fb67326577e8ee97af
geraldvalente01@gmail.com <geraldvalente01@gmail.com>
2025-04-21 07:44:17 +0800

    pede na sa index.html

---
+++ site/pages/donut.js
@@ -0,0 +1,23 @@
+const soloBtn = document.querySelector('.menu-type span:nth-child(1)');
+const bundleBtn = document.querySelector('.menu-type span:nth-child(3)');
+const soloContainer = document.getElementById('regular-solo');
+const bundleContainer = document.getElementById('regular-bundle');
+
+function showSolo() {
+    bundleContainer.style.display = 'none';
+    soloContainer.style.display = 'flex';
+    soloContainer.classList.remove('slide-in-right');
+    void soloContainer.offsetWidth;
+    soloContainer.classList.add('slide-in-left');
+}
+
+function showBundle() {
+    soloContainer.style.display = 'none';
+    bundleContainer.style.display = 'flex';
+    bundleContainer.classList.remove('slide-in-left');
+    void bundleContainer.offsetWidth;
+    bundleContainer.classList.add('slide-in-right');
+}
+
+soloBtn.addEventListener('click', showSolo);
+bundleBtn.addEventListener('click', showBundle);
