8ea5bfc2293de23db55a70e699a9e06da8379410
Jzy <jestalycastillo15@gmail.com>
2025-05-16 13:01:31 +0800

    Fix: order insertion

--- admin/boards/complete_orders.php
+++ admin/boards/complete_orders.php
@@ -8,6 +8,17 @@ if (isset($_GET['order_id'])) {
     $order_update_query = "UPDATE orders SET order_status = 'completed' WHERE order_id = $order_id;";
     $order_update_query_run = mysqli_query($conn, $order_update_query);
 
+    $user_id_query = "SELECT * FROM orders WHERE order_id = $order_id;";
+    $user_id_query_run = mysqli_query($conn, $user_id_query);
+    $user_id_row = mysqli_fetch_assoc($user_id_query_run);
+
+    $user_id = $user_id_row['user_id'];
+    $message = "You order is now ready!";
+    $date = date("Y-m-d");
+
+    $notif_insert_query = "INSERT INTO notification(user_id, message, order_date) VALUES($user_id, '$message', '$date');";
+    $notif_insert_query_run = mysqli_query($conn, $notif_insert_query);
+
     header('Content-Type: application/json');
     echo json_encode($order_id);
 }
--- user/boards/send_orders.php
+++ user/boards/send_orders.php
@@ -8,16 +8,16 @@ if (isset($_GET['user_id'])) {
     $order_insert_query = "INSERT INTO orders(user_id, order_date, order_status) VALUES($user_id, '" . date("Y-m-d") . "', 'pending');";
     $order_insert_query_run = mysqli_query($conn, $order_insert_query);
 
-    $user_cart_query = "SELECT cart.*, orders.order_id FROM cart LEFT JOIN orders ON cart.user_id = orders.user_id WHERE cart.user_id = $user_id;";
+    $user_cart_query = "SELECT cart.*, orders.order_id FROM cart LEFT JOIN orders ON cart.user_id = orders.user_id WHERE cart.user_id = $user_id AND orders.order_status = 'pending';";
     $user_cart_query_run = mysqli_query($conn, $user_cart_query);
 
     while ($user_cart_row = mysqli_fetch_assoc($user_cart_query_run)) {
-        $order_id = empty($user_cart_row['order_id']) ? NULL : $user_cart_row['order_id'];
-        $coffee_id = empty($user_cart_row['coffee_id']) ? NULL : $user_cart_row['coffee_id'];
-        $coffee_size_id = empty($user_cart_row['coffee_size_id']) ? NULL : $user_cart_row['coffee_size_id'];
-        $donut_id = empty($user_cart_row['donut_id']) ? NULL : $user_cart_row['donut_id'];
-        $bundle_id = empty($user_cart_row['bundle_id']) ? NULL : $user_cart_row['bundle_id'];
-        $quantity = empty($user_cart_row['quantity']) ? NULL : $user_cart_row['quantity'];
+        $order_id = $user_cart_row['order_id'];
+        $coffee_id = $user_cart_row['coffee_id'];
+        $coffee_size_id = $user_cart_row['coffee_size_id'];
+        $donut_id = $user_cart_row['donut_id'];
+        $bundle_id = $user_cart_row['bundle_id'];
+        $quantity = $user_cart_row['quantity'];
 
         $order_details_insert_query = "INSERT INTO order_details(
         order_id, coffee_id, coffee_size_id, donut_id, bundle_id, quantity) VALUES (
